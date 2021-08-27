755629d1738ace5bf45aff8640065e2b1f9f58c4
MouadBNL <mouadbnl.2k01@gmail.com>
2021-08-27 11:46:52 +0100

    Edditing all possible options

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # NPM First package for learning
 Trying to create a package on NPM
 
+## Instalation
+```bash
+npm i mouadbnl-first-package --save
+```
+
+## Usage
+First define where you want to apply the sahdows
+```html
+<div m-sw style="padding = 5px;">
+    lorem
+</div>
+```
+
+then Import and initialize the wizard
 ```javascript
 import { shadowizard } from "shadowizard"
 
@@ -8,3 +22,4 @@ shadowizard({
     shadow_type: 'hard'
 });
 ```
+And it works
--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-function shadowizard(options)
+function shadowizard({
+    x= "5px",
+    y= "5px",
+    blur= "10px",
+    spread= "0px",
+    color= "rgba(0,0,0,.2)",
+    inset= false,
+
+    shadow_type=null
+} = {})
 {
-    let shadow_params = {
-        x: "0px",
-        y: "0px",
-        blur: "10px",
-        spread: "0px",
-        color: "rgba(0,0,0,.2)",
-        inset: false
-    };
     let dom = document.querySelectorAll('[m-sw]');
 
     if(options.shadow_type === "hard")
-    {
-        shadow_params.blur = "0px";
-    }
+        blur = "0px";
 
     dom.forEach(el => {
-        el.style.boxShadow = `${shadow_params.inset ? 'inset' : ''} ${shadow_params.x} ${shadow_params.y} ${shadow_params.blur}  ${shadow_params.spread}  ${shadow_params.color}`;
+        el.style.boxShadow = `${inset ? 'inset' : ''} ${x} ${y} ${blur}  ${spread}  ${color}`;
     });
 }
 
